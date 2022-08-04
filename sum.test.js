4c32251a9521a34b3e0fa3e27bbb49ea5de40d19
Mark Parris <audio_dev@hotmail.com>
2022-08-04 13:29:30 -0700

    Try axios pre-release version

--- sum.test.js
+++ sum.test.js
@@ -6,7 +6,7 @@ test('adds 1 + 2 to equal 3', () => {
 });
 
 test('axios test', async () => {
-  // const response = await axios.get('https://www.google.com/not_found');
-  const response = await fetch('https://www.google.com/not_found');
+  const response = await axios.get('https://www.google.com/not_found');
+  // const response = await fetch('https://www.google.com/not_found');
   console.log(response);
 });
